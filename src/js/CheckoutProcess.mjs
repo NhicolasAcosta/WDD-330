33b67fea41295ebbf40c8feab37cd9e202446873
Yeampier2022 <yeampierjesusa@gmail.com>
2023-10-04 14:28:26 -0400

    feat: calculateOrdertotal

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -27,7 +27,6 @@ function packageItems(items) {
   return simplifiedItems;
 }
 
-
 export default class CheckoutProcess {
   constructor(key, outputSelector) {
     this.key = key;
@@ -40,22 +39,25 @@ export default class CheckoutProcess {
   }
 
   init() {
-    this.list = getLocalStorage(this.key);
+    this.list = Object.values(getLocalStorage(this.key));
     this.calculateItemSummary();
+    this.calculateOrdertotal();
+    this.displayOrderTotals();
   }
 
   calculateItemSummary() {
     // calculate and display the total amount of the items in the cart, and the number of items.
     const summaryElement = document.querySelector(
-      this.outputSelector + " #cartTotal")
-      const itemNumElement = document.querySelector(
-        this.outputSelector + " #num-items"
-      );
-      itemNumElement.innerText = this.list.length;
-      // calculate the total of all the items in the cart
-      const amounts = this.list.map((item) => item.FinalPrice);
-      this.itemTotal = amounts.reduce((sum, item) => sum + item);
-      summaryElement.innerText = "$" + this.itemTotal;
+      this.outputSelector + " #cartTotal"
+    );
+    const itemNumElement = document.querySelector(
+      this.outputSelector + " #num-items"
+    );
+    itemNumElement.innerText = this.list.length;
+    // calculate the total of all the items in the cart
+    const amounts = this.list.map((item) => item.FinalPrice * item.quantity);
+    this.itemTotal = amounts.reduce((sum, item) => sum + item).toFixed(2);
+    summaryElement.innerText = "$" + this.itemTotal;
   }
 
   calculateOrdertotal() {
@@ -82,7 +84,6 @@ export default class CheckoutProcess {
     shipping.innerText = "$" + this.shipping;
     tax.innerText = "$" + this.tax;
     orderTotal.innerText = "$" + this.orderTotal;
-
   }
 
   async checkout() {
